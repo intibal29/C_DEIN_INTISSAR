c5c369ad3a092c940db3b46c7e9d0fa15ab04869
intibal29 <baloukintissar@gmail.com>
2024-10-17 13:12:55 +0200

    Persona modificada

--- src/main/java/org/example/C/Persona.java
+++ src/main/java/org/example/C/Persona.java
@@ -2,65 +2,41 @@ package org.example.C;
 
 import java.util.Objects;
 
-/**
- * La clase {@code Persona} representa una persona con un nombre, apellidos y edad.
- * Esta clase incluye métodos para acceder a las propiedades de la persona y
- * sobrescribe los métodos {@code equals} y {@code hashCode} para permitir la
- * comparación de objetos de tipo {@code Persona}.
- */
 public class Persona {
     private String nombre;
     private String apellidos;
     private int edad;
 
-    /**
-     * Crea una nueva instancia de {@code Persona} con el nombre, apellidos y edad especificados.
-     *
-     * @param nombre    el nombre de la persona
-     * @param apellidos los apellidos de la persona
-     * @param edad      la edad de la persona
-     */
     public Persona(String nombre, String apellidos, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
     }
 
-    /**
-     * Obtiene el nombre de la persona.
-     *
-     * @return el nombre de la persona
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * Obtiene los apellidos de la persona.
-     *
-     * @return los apellidos de la persona
-     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getApellidos() {
         return apellidos;
     }
 
-    /**
-     * Obtiene la edad de la persona.
-     *
-     * @return la edad de la persona
-     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
     public int getEdad() {
         return edad;
     }
 
-    /**
-     * Compara este objeto {@code Persona} con el objeto especificado para ver si son iguales.
-     * Dos objetos son considerados iguales si tienen el mismo nombre, apellidos y edad.
-     *
-     * @param o el objeto a comparar con esta {@code Persona}
-     * @return {@code true} si el objeto especificado es igual a esta {@code Persona},
-     * {@code false} en caso contrario
-     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -71,11 +47,6 @@ public class Persona {
                 Objects.equals(apellidos, persona.apellidos);
     }
 
-    /**
-     * Devuelve un valor de hash para esta {@code Persona}.
-     *
-     * @return un valor de hash para esta {@code Persona}
-     */
     @Override
     public int hashCode() {
         return Objects.hash(nombre, apellidos, edad);
